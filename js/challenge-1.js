42a88f988071f7213906f83ae64ff67a8e984ed6
Richard Wright <richard@commonplace.is>
2021-03-25 07:31:35 +0000

    tidy up

--- js/challenge-1.js
+++ js/challenge-1.js
@@ -1,24 +1,9 @@
-function toggleDynamicBasket() {
-    const cartButton = document.getElementById('add-to-cart-button');
-    const cartButtonPosition = cartButton.getBoundingClientRect(); 
-    const dynamicBasket = document.getElementById('dynamic-basket');
-
-    if (cartButtonPosition.top >= 0 && cartButtonPosition.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
-        dynamicBasket.classList.remove('slide-in');
-        dynamicBasket.classList.add('slide-out');
-    } else if (dynamicBasket.classList.contains('slide-out')){
-        dynamicBasket.classList.remove('slide-out');
-        dynamicBasket.classList.add('slide-in');
-    }
-}
-
 function addDynamicBasket() {
-    const wrapper = document.createElement('div');
-    wrapper.id = 'dynamic-basket-wrapper';
-
+    // make outer div
     const dynamicBasket = document.createElement('div');
     dynamicBasket.id = 'dynamic-basket';
 
+    // make select
     const quantitySelect = document.createElement('select');
     quantitySelect.id = 'dynamic-basket-select';
     quantitySelect.selectedIndex = 0;
@@ -30,63 +15,62 @@ function addDynamicBasket() {
     option2.setAttribute('value', '2');
     const option2Text = document.createTextNode('2');
     option2.appendChild(option2Text);
+
     quantitySelect.appendChild(option1);
     quantitySelect.appendChild(option2);
 
-    dynamicBasket.appendChild(quantitySelect);
-
+    // make add to basket button
     const buttonWrapper = document.createElement('div');
     buttonWrapper.classList.add('a-button-stack');
-
     const buttonOuterSpan = document.createElement('span');
     buttonOuterSpan.classList.add('a-declarative');
-
     const buttonMiddleSpan = document.createElement('span');
     buttonMiddleSpan.classList.add('a-button', 'a-button-primary', 'a-button-icon');
-
     const buttonInnerSpan = document.createElement('span');
     buttonInnerSpan.classList.add('a-button-inner');
-
     const buttonIcon = document.createElement('i');
     buttonIcon.classList.add('a-icon', 'a-icon-cart');
-
     const buttonInput = document.createElement('input');
     buttonInput.classList.add('a-button-input');
     buttonInput.setAttribute('value', 'Add to Basket');
     buttonInput.setAttribute('title', 'Add to Shopping Basket');
     buttonInput.setAttribute('aria-labelledby', 'submit.add-to-cart-announce');
-    buttonInput.addEventListener('click', submitAddToCart)
-
+    // screen reader text
     const buttonSRtext = document.createElement('span');
     buttonSRtext.classList.add('a-button-text');
     buttonSRtext.setAttribute('aria-hidden', 'true');
-
     const buttonSRtextNode = document.createTextNode('Add to Basket');
     buttonSRtext.appendChild(buttonSRtextNode);
 
-    dynamicBasket.appendChild(buttonWrapper);
     buttonWrapper.appendChild(buttonOuterSpan);
     buttonOuterSpan.appendChild(buttonMiddleSpan);
     buttonMiddleSpan.appendChild(buttonInnerSpan);
-
     [buttonIcon, buttonInput, buttonSRtext].forEach((element) => buttonInnerSpan.appendChild(element));
 
-    wrapper.appendChild(dynamicBasket);
-
+    // append select and add to basket button to outer div, and outer div to body
+    dynamicBasket.appendChild(quantitySelect);
+    dynamicBasket.appendChild(buttonWrapper);
     const bodyQuery = document.getElementsByTagName('body');
-    bodyQuery[0].appendChild(wrapper);
+    bodyQuery[0].appendChild(dynamicBasket);
 
+    // add event listeners
     document.addEventListener('scroll', toggleDynamicBasket);
-
     quantitySelect.addEventListener('change', updateQuantities);
+    buttonInput.addEventListener('click', submitAddToCart);
 }
 
-function checkForBodyTag(mutations, observer) {
-    const body = document.getElementsByTagName('body');
+// only show the dynamic basket if the main add to basket button is outside the view port
+function toggleDynamicBasket() {
+    const addToBasketButton = document.getElementById('add-to-cart-button');
+    const addToBasketButtonPosition = addToBasketButton.getBoundingClientRect(); 
+    const dynamicBasket = document.getElementById('dynamic-basket');
 
-    if(body.length) {
-        addDynamicBasket();
-        return observer.disconnect();
+    if (addToBasketButtonPosition.top >= 0 && addToBasketButtonPosition.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
+        dynamicBasket.classList.remove('slide-in');
+        dynamicBasket.classList.add('slide-out');
+    } else if (dynamicBasket.classList.contains('slide-out')){
+        dynamicBasket.classList.remove('slide-out');
+        dynamicBasket.classList.add('slide-in');
     }
 }
 
@@ -105,5 +89,15 @@ function submitAddToCart() {
     addToCartForm.submit();
 }
 
+function checkForBodyTag(mutations, observer) {
+    const body = document.getElementsByTagName('body');
+
+    if(body.length) {
+        addDynamicBasket();
+        return observer.disconnect();
+    }
+}
+
+// make sure body tag has loaded before appending to it
 const observer = new MutationObserver(checkForBodyTag);
 observer.observe(document, { childList: true, subtree: true });
